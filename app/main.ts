4fcbed5ab720791595d0378d4440a920a8176647
dabao <scarecrow_7337@sohu.com>
2016-05-23 13:48:54 +0800

    init

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,5 @@
 import {bootstrap}  from 'angular2/platform/browser';
-import 'rxjs/Rx';
-import {enableProdMode}   from 'angular2/core';
+//import {enableProdMode}   from 'angular2/core';
 import {AppComponent}   from './app.component';
 
 //enableProdMode();
